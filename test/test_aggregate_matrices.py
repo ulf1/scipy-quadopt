494811a516bcfa964f8e4b5cbe702b493b730d88
UH <554c46@gmail.com>
2021-04-07 17:04:24 +0200

    an unit test added

--- test/test_aggregate_matrices.py
+++ test/test_aggregate_matrices.py
@@ -25,3 +25,13 @@ def test4():
     beta2 = 0.5
     simi = sqp.aggregate_matrices(simi1, beta1, simi2, beta2)
     np.testing.assert_equal(simi, [[4.5, 7], [9.5, 12]])
+
+
+def test5():
+    simi1 = [[1, 2], [3, 4]]
+    beta1 = 2
+    simi2 = [[5, 6], [7, 8]]
+    beta2 = 0.5
+    simi = sqp.aggregate_matrices(simi1, beta1, simi2, beta2, dtype=np.float16)
+    assert simi.dtype == np.float16
+    np.testing.assert_equal(simi, [[4.5, 7], [9.5, 12]])
